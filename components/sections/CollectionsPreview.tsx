20162f3098054c78b09aba1c942b41488e72fd6a
zeynep sıla yılmaz <zeynepsilayilmaz@zeynep-MacBook-Pro.local>
2026-06-05 15:47:58 +0300

    Fix: add use client to components using useTranslations

--- components/sections/CollectionsPreview.tsx
+++ components/sections/CollectionsPreview.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { useTranslations } from 'next-intl'
 import { ScrollReveal } from '@/components/motion/ScrollReveal'
--- components/sections/WhatsAppCTA.tsx
+++ components/sections/WhatsAppCTA.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useTranslations } from 'next-intl'
 import { ScrollReveal } from '@/components/motion/ScrollReveal'
 import { createWhatsAppLink } from '@/lib/whatsapp'
