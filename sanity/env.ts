cce57c56340a48c9607730301a1e65bf4f532ee6
zeynep sıla yılmaz <zeynepsilayilmaz@zeynep-MacBook-Pro.local>
2026-06-05 14:54:37 +0300

    Fix: clean dependency rewrite

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,8 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-06-02'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ''
